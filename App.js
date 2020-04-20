cff5d180924e1a0bb7bc8bed499d74d79dc1703b
MarcoRas04 <mnras04@gmail.com>
2020-04-20 14:39:16 +0200

    new style button

--- App.js
+++ App.js
@@ -1,7 +1,2 @@
-const names = ["Ed", "Harry", "Marco", "Herman", "Anika", "Penutter"];
-var loading = 0;
-
-while (loading < 100) {
-  console.log(loading);
-  loading++;
-}
+const text = document.querySelector(".title");
+const changeColor = document.querySelector(".changeColor");
