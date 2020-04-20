3ac686c0bb43c6fcef0bc10813c017d160dc85ca
MarcoRas04 <mnras04@gmail.com>
2020-04-20 11:15:47 +0200

    If and else statements

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const name = "Marco";
-const  age = 32;
-console.log(`Hello my name is ${name} and my age is ${age}`);
+const age = 10;
+if(age > 18){
+console.log(`Wow you are ${age} years old, you qualify to be a costumer`)
+}else{
+console.log(`Wow you are ${age} years old, that is too young but will get there`)
+}
