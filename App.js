1a01443d62e70b3dbe1fb1e12f770ca5c7302952
MarcoRas04 <mnras04@gmail.com>
2020-04-20 14:13:30 +0200

    Dom Manupulation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const names = ["Ed", "Harry", "Marco", "Herman", "Anika", "Penutter"];
-var counter = 0;
+var loading = 0;
 
-while (loadnig < 100) {
-  Loading++;
+while (loading < 100) {
+  console.log(loading);
+  loading++;
 }
