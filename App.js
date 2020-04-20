2b4c87ef2806bb0d09406f8684121717eb6a815f
MarcoRas04 <mnras04@gmail.com>
2020-04-20 11:40:57 +0200

    Arrays

--- App.js
+++ App.js
@@ -1,12 +1,1 @@
-const age = 19;
-if (age > 18) {
-  console.log(`Wow you are ${age} years old, you qualify to be a costumer`);
-} else if (age < 13) {
-  console.log(
-    `Wow you are ${age} years old, why are you allowed on the internet`
-  );
-} else {
-  console.log(
-    `Wow you are ${age} years old, you are almost old enough see you soon`
-  );
-}
+const schedules = ["Wake up", "Eat", "Netflix"];
