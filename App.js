5ea2eeb5ba7733bdcbe61302c96ecb3f1fa9c130
MarcoRas04 <mnras04@gmail.com>
2020-04-20 12:19:25 +0200

    Keyword this

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-Const user = {
-    name: "Marco",
-    age: 24,
-    Married: true,
-    Purchases: ["Phone" , "Car" , "Laptop"]
+const user = {
+  name: "Marco",
+  age: 24,
+  Married: true,
+  Purchases: ["Phone", "Car", "Laptop"],
 };
 
 console.log(user.Purchases);
 
+//THIS
