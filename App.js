12c4d8fe5325419aacb1db2792c5e8fbcbf024bc
MarcoRas04 <mnras04@gmail.com>
2020-04-20 11:23:52 +0200

    Nested if statements

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-const age = 10;
-if(age > 18){
-console.log(`Wow you are ${age} years old, you qualify to be a costumer`)
-}else{
-console.log(`Wow you are ${age} years old, that is too young but will get there`)
+const age = 19;
+if (age > 18) {
+  console.log(`Wow you are ${age} years old, you qualify to be a costumer`);
+} else if (age < 13) {
+  console.log(
+    `Wow you are ${age} years old, why are you allowed on the internet`
+  );
+} else {
+  console.log(
+    `Wow you are ${age} years old, you are almost old enough see you soon`
+  );
 }
