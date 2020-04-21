0d8c1bbcd3d5216eb95d8251f966e05a8d8aa309
MarcoRas04 <mnras04@gmail.com>
2020-04-21 09:05:34 +0200

    Done

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-const userList = document.querySelectorAll(".name-List li");
+const userList = document.querySelector(".name-List li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
 
-console.log(userList);
-
-for (user of userList) {
-  user.addEventListener("Click", function () {
-    console.log(this);
-  });
-}
+addListBtn.addEventListener("click", function () {
+  //Create an Li
+  const newLi = document.createElement("LI");
+  const liContent = document.createTextNode(listInput.value);
+  //Add the input into the Li
+  newLi.appendChild(liContent);
+  //Attach to user list already existing
+  userList.appendChild(newLi);
+});
